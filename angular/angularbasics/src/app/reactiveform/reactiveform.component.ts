5d96e756e6aa452158c217283c66bcb9640281e3
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-04-09 08:09:44 +0530

    custom validations

--- angular/angularbasics/src/app/reactiveform/reactiveform.component.ts
+++ angular/angularbasics/src/app/reactiveform/reactiveform.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { formControlBinding } from '@angular/forms/src/directives/ng_model';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+
+
+function phoneLengthValidation (c:AbstractControl):{[key:string]:boolean} | null{
+  if( c.value.toString().length == 10 ){
+    return null;
+  }else{
+    return {
+      match:true
+    }
+  }
+}
+  
 
 @Component({
   selector: 'app-reactiveform',
@@ -8,15 +19,19 @@ import { formControlBinding } from '@angular/forms/src/directives/ng_model';
   styleUrls: ['./reactiveform.component.css']
 })
 export class ReactiveformComponent implements OnInit {
-  customerInfo:FormGroup
+  customerInfo:FormGroup;
   constructor() { }
 
   ngOnInit() {
     this.customerInfo = new FormGroup({
       firstName: new FormControl('',Validators.required),
       lastName: new FormControl('',Validators.required),
-      emailControl: new FormControl('',[Validators.required,Validators.email]),
-      checkin: new FormControl('true')
+      emailGroup: new FormGroup({
+        emailControl: new FormControl('',[Validators.required,Validators.email]),
+        confirmEmailControl: new FormControl('',[Validators.required,Validators.email]),
+      }),
+      checkin: new FormControl('true'),
+      phoneControl: new FormControl('',phoneLengthValidation)
       // password: new FormControl('',[Validators.required,Validators.minLength(6)]),
 
 
