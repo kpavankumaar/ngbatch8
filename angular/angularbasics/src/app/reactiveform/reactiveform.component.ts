456e454f88edcb2ad5253c25b6e990b99f98fa98
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-04-16 08:13:50 +0530

    cross field validations

--- angular/angularbasics/src/app/reactiveform/reactiveform.component.ts
+++ angular/angularbasics/src/app/reactiveform/reactiveform.component.ts
@@ -1,7 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
-
+function verifyEmail(c:AbstractControl):{[key:string]:Boolean} | null {
+  
+  let emailControl = c.get('emailControl');
+  let confirmEmailControl = c.get('confirmEmailControl');
+  console.log('verify Email');
+  if(emailControl.value == confirmEmailControl.value){
+    return null;
+  }else{
+    return {
+      emailComparison: true
+    }
+  }
+}
+function ratingValidator(){
+  return function ratingCheck(c:AbstractControl):{[key:string]:boolean} | null{
+    if(c.value > 0 && c.value < 6){
+      return null
+    }else{
+      return {
+        ratingMatch: true
+      }
+    }
+  }
+}
 function phoneLengthValidation (c:AbstractControl):{[key:string]:boolean} | null{
   if( c.value.toString().length == 10 ){
     return null;
@@ -29,12 +52,11 @@ export class ReactiveformComponent implements OnInit {
       emailGroup: new FormGroup({
         emailControl: new FormControl('',[Validators.required,Validators.email]),
         confirmEmailControl: new FormControl('',[Validators.required,Validators.email]),
-      }),
+      },verifyEmail),
       checkin: new FormControl('true'),
-      phoneControl: new FormControl('',phoneLengthValidation)
+      phoneControl: new FormControl('',phoneLengthValidation),
       // password: new FormControl('',[Validators.required,Validators.minLength(6)]),
-
-
+      rating:new FormControl('',ratingValidator())
     })
   }
   sendData(){
